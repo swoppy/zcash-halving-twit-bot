3ac7e7f1f37a65e87411a06474c35735c7c5ba83
swoppy <json.barba@gmail.com>
2020-05-27 16:17:32 +0800

    bot is now able to upload photo from a site screenshot and tweet it on specific interval

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 require('dotenv').config();
 const Twit = require('twit');
+const text = require('./src/text');
+const { webkit } = require('playwright');
+const fs = require('fs');
 
 const T = new Twit({
   consumer_key:         process.env.TWITTER_CONSUMER_KEY,
   consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
   access_token:         process.env.TWITTER_ACCESS_TOKEN,
   access_token_secret:  process.env.TWITTER_ACCESS_TOKEN_SECRET,
-})
-
-T.post('statuses/update', { status: 'from backend test!!' }, function(err, data, response) {
-  console.log(data)
 });
 
+const path = 'src/img/site.png';
+
+(async () => {
+  const browser = await webkit.launch();
+  const page = await browser.newPage();
+  await page.goto(text.siteLink);
+  await page.screenshot({ path: path });
+  await browser.close();
+})();
+
+const tweetedCb = (err, data, response) => {
+  if (err) {
+    console.log('error: something wrong!');
+  }
+  else {
+    console.log(`Tweet posted! date: ${data.created_at}`);
+  }
+}
+
+const uploadedCb = (err, data, response) => {
+  const id = data.media_id_string;
+  const post = {
+    status: text.status(),
+    media_ids: [id],
+  };
+  T.post('statuses/update', post, tweetedCb);
+};
+
+const upload = () => {
+  const b64Media = fs.readFileSync(path, { encoding: 'base64' }); 
+  T.post('media/upload', { media_data: b64Media }, uploadedCb);
+}
+
+upload();
+// setInterval(upload, 140000000);
---
+++ src/text.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteLink: 'https://zcashblockhalf.com',
+  status: () => `Catchprase\n\nBuy & Trade at https://accounts.binance.com/en/register?ref=FOPFM9RP\nmore details regarding the halving at https://zcashblockhalf.com`,
+};
