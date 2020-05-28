dfb0d12c7f93ed0a8135a9f827b3b38d18f1a7de
swoppy <json.barba@gmail.com>
2020-05-28 21:21:43 +0800

    Set viewport deviceScaleFactor to help reduce fuzzy sreenshot

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ function upload() {
     try {
       const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
       const page = await browser.newPage();
+      await page.setViewport({width: 766, height: 113, deviceScaleFactor: 2});
       await page.goto(text.siteLink);
       await page.waitForSelector('#countdownTimer')
       const element = await page.$('#countdownTimer');
